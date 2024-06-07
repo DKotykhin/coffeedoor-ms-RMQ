42392c1ae3257791f1e79edd54c531b38892c8ae
Dmytro Kotykhin <kotykhin_d@ukr.net>
2024-06-07 23:31:43 +0200

    start kubernetes

--- coffeedoor-api-graphql/src/main.ts
+++ coffeedoor-api-graphql/src/main.ts
@@ -11,9 +11,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
+
   const configService = app.get(ConfigService);
   const PORT = configService.get<string>('HTTP_PORT');
   await app.listen(PORT);
+
   logger.log(`API GraphQL Gateway service is running on port ${PORT}`);
 }
 bootstrap();
